c9d26141eca8e5843ba3e25564f29ddf7447c05d
InaYarashevich <ina.yarashevich@gmail.com>
2022-05-09 16:09:10 +0300

    refactoring project

--- src/test/java/homework/day17/SelectTest.java
+++ src/test/java/homework/day17/SelectTest.java
@@ -1,26 +1,15 @@
 package homework.day17;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class SelectTest {
     static WebDriver driver = new ChromeDriver();
 
-    @Before
-    public void setUp(){
-        SelectTest selectTest = new SelectTest();
-
-    }
-
-    @Test
     public static void main(String[] args) {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
@@ -28,32 +17,42 @@ public class SelectTest {
         driver.manage().window().maximize();
 
         driver.get("https://demoqa.com/select-menu");
+
         WebElement firstSelect = driver.findElement(By.id("withOptGroup"));
         firstSelect.click();
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[contains(., 'Group 1, option 1')]"))))
-                .click();
+        Actions selectOption = new Actions(driver);
+        selectOption
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .build().perform();
 
         WebElement secondSelect = driver.findElement(By.id("selectOne"));
         secondSelect.click();
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//*[contains(., 'Ms.')]"))))
-                .click();
+        Actions selectOption2 = new Actions(driver);
+        selectOption2
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .build().perform();
 
         WebElement thirdSelect = driver.findElement(By.id("oldSelectMenu"));
         thirdSelect.click();
         Select oldStyleColorSelect = new Select(driver.findElement(By.id("oldSelectMenu")));
         oldStyleColorSelect.selectByIndex(3);
 
-        WebElement fourthSelect = driver.findElement(By.id("//*[text()='Multiselect drop down']"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", fourthSelect);
+        WebElement fourthSelect = driver.findElement(By.xpath("//*[text()='Multiselect drop down']/../following-sibling::div"));
         fourthSelect.click();
+        Actions selectOption4 = new Actions(driver);
+        selectOption4
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .sendKeys(Keys.ARROW_DOWN)
+                .sendKeys(Keys.ENTER)
+                .build().perform();
 
         Select fifthSelect = new Select(driver.findElement(By.id("cars")));
         fifthSelect.selectByIndex(3);
-    }
 
-    @After
-    public void teardown(){
         driver.quit();
     }
 }
