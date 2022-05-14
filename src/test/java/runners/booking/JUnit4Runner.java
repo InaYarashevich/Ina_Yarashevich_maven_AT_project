4f5e0fc281fc212e8e999a2595f3c318c1203b3a
InaYarashevich <ina.yarashevich@gmail.com>
2022-05-14 17:15:13 +0300

    added TestNGXml Runner

--- src/test/java/runners/booking/JUnit4Runner.java
+++ src/test/java/runners/booking/JUnit4Runner.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        
+
 })
 
 public class JUnit4Runner {
---
+++ src/test/java/runners/booking/TestNGXmlSuite.java
@@ -0,0 +1,19 @@
+package runners.booking;
+
+import org.testng.TestNG;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.internal.Parser;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TestNGXmlSuite {
+
+    public static void main(String[] args) throws IOException {
+        final TestNG testNG = new TestNG(true);
+        final Parser parser = new Parser("");
+        final List<XmlSuite> suites = parser.parseToList();
+        testNG.setXmlSuites(suites);
+        testNG.run();
+    }
+}
